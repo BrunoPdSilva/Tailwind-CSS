fd55ddff1ad8d965c8f3785acc84756a6883735b
Bruno Peres <bruno_16515@hotmail.com>
2022-07-28 17:02:18 -0300

    Aula 05 concluída

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     './public/**/*.{html,js}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883'
+        }
+      }
+    },
   },
   plugins: [],
 }
